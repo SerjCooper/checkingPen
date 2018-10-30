0ee55e5c88de4d7a467a8c6fd6935a07ffa953e5
Serj Cooper <b-sergi@mail.ru>
2018-10-30 16:57:15 +0500

    Добавил 7 тестов. Фиксируется 1 баг

--- src/test/java/TestPen.java
+++ src/test/java/TestPen.java
@@ -1,28 +1,93 @@
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import static org.testng.Assert.*;
 
 public class TestPen {
 
-    @org.testng.annotations.BeforeMethod
+    @BeforeMethod
     public void setUp() {
+
     }
 
-    @org.testng.annotations.AfterMethod
+    @AfterMethod
     public void tearDown() {
     }
 
-    @org.testng.annotations.Test
-    public void testWrite() {
+    @Test
+    public void testWrite_isWorkFalse() {
+        int inkContainerValue = 0;                  //ожидаем выполнения условия !isWork
+        String word = "word";
+        String response = "";                        //ожидаемый результат
+
+        Pen pen = new Pen(inkContainerValue);
+        assertEquals(pen.write(word), response);
     }
 
-    @org.testng.annotations.Test
+    @Test
+    public void testWrite_sizeOfWordTrue_defaultSize() {
+        String word = "word";
+        int inkContainerValue = word.length() + 1;  //ожидаем пропуск условия !isWork и выполнения условия sizeOfWord<=inkContainerValue
+
+        Pen pen = new Pen(inkContainerValue);       //создаем Pen со значением sizeOfWord по умолчанию
+        assertEquals(pen.write(word), word);        //проверяем, что условие sizeOfWord<=inkContainerValue выполняется и метод возращает word
+    }
+
+    @Test
+    public void testWrite_sizeOfWordFalse_defaultSize() {
+        String word = "word";
+        int inkContainerValue = 1;  //ожидаем пропуск условия !isWork и выполнения условия sizeOfWord<=inkContainerValue
+
+        Pen pen = new Pen(inkContainerValue);       //создаем Pen со значением sizeOfWord по умолчанию
+        assertEquals(pen.write(word), word.substring(0, inkContainerValue));        //проверяем, что условие sizeOfWord<=inkContainerValue НЕ выполняется и метод возращает первую букву word
+    }
+
+    @Test
     public void testGetColor() {
+        String color = "RED";
+
+        Pen pen = new Pen(1, 1.0, color);
+        assertEquals(color , pen.getColor());
+    }
+    //***********Тестирование метода isWork()***************
+    @Test
+    public void testIsWork_containerPositive() {
+        int inkContainerValue = 1;
+
+        Pen pen = new Pen(inkContainerValue);
+        assertTrue(pen.isWork());
     }
 
-    @org.testng.annotations.Test
-    public void testIsWork() {
-    }
+    @Test
+    public void testIsWork_containerEmpty() {
+        int inkContainerValue = 0;
 
-    @org.testng.annotations.Test
-    public void testDoSomethingElse() {
+        Pen pen = new Pen(inkContainerValue);
+        assertFalse(pen.isWork());
+    }
+    //***********Тестирование метода isWork()***************
+
+
+    @Test
+    public void testDoSomethingElse() throws IOException {
+        int inkContainerValue = 1000;
+        double sizeLetter = 1.0;
+        String color = "RED";
+
+        Pen pen = new Pen (inkContainerValue, sizeLetter, color);
+
+        File file = new File("temp.txt"); // создаем файл
+        PrintStream ps = new PrintStream(file); // создаем поток вывода в файл
+        PrintStream standardOut = System.out; // сохраняем стандартный поток вывода
+        System.setOut(ps); // присваиваем файловый поток в качестве основного
+        pen.doSomethingElse(); // вызываем метод
+        Assert.assertEquals(Files.readAllLines(Paths.get(file.toURI())).get(0), color); // проверяем что результат в файле равен ожидаемому
+        System.setOut(standardOut); // возвращаем метод в исходное состояние
     }
 }
