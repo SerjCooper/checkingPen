a0d8bd585fd9aad7a34092b32156e3a1fd25af87
Serj Cooper <b-sergi@mail.ru>
2018-10-30 14:52:36 +0500

    Initial commit

---
+++ src/test/java/TestPen.java
@@ -0,0 +1,28 @@
+import static org.testng.Assert.*;
+
+public class TestPen {
+
+    @org.testng.annotations.BeforeMethod
+    public void setUp() {
+    }
+
+    @org.testng.annotations.AfterMethod
+    public void tearDown() {
+    }
+
+    @org.testng.annotations.Test
+    public void testWrite() {
+    }
+
+    @org.testng.annotations.Test
+    public void testGetColor() {
+    }
+
+    @org.testng.annotations.Test
+    public void testIsWork() {
+    }
+
+    @org.testng.annotations.Test
+    public void testDoSomethingElse() {
+    }
+}
